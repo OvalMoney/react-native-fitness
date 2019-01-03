88983991aea9b7d105c7369bf07a472a3b3ed734
francesco.voto <fv@ovalmoney.com>
2019-01-03 12:55:16 +0100

    Read steps from GMS to have same value with Google Fit

--- android/src/main/java/com/ovalmoney/fitness/manager/Manager.java
+++ android/src/main/java/com/ovalmoney/fitness/manager/Manager.java
@@ -92,8 +92,15 @@ public class Manager implements ActivityEventListener {
     public void onNewIntent(Intent intent) { }
 
     public void getSteps(Context context, double startDate, double endDate, final Promise promise){
+        DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setType(DataSource.TYPE_DERIVED)
+                .setStreamName("estimated_steps")
+                .setAppPackageName("com.google.android.gms")
+                .build();
+
         DataReadRequest readRequest = new DataReadRequest.Builder()
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .aggregate(ESTIMATED_STEP_DELTAS,    DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .bucketByTime(1, TimeUnit.DAYS)
                 .setTimeRange((long) startDate, (long) endDate, TimeUnit.MILLISECONDS)
                 .build();
