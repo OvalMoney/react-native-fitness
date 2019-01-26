fb71dde43c7a9587e3dd64dd5faea2ef3ff64098
tallpants <adithyakreddy6@gmail.com>
2019-01-26 11:11:23 +0530

    dispatch_once implementation for NSISO8601DateFormatter

--- ios/RCTFitness/RCTFitness+Utils.m
+++ ios/RCTFitness/RCTFitness+Utils.m
@@ -11,27 +11,25 @@
 #include <time.h>
 
 @implementation RCTFitness(Utils)
-    
-+ (NSString *)ISO8601StringFromDate: (NSDate*) date {
-    // Cache the formatter in thread local storage the first
-    // time it's created and then re-use it every other time.
-    NSString *cachedISO8601DateFormatterKey = @"cachedNSISO8601DateFormatterKey";
-    NSMutableDictionary *threadDictionary = [[NSThread currentThread] threadDictionary];
-    NSISO8601DateFormatter *dateFormatter = threadDictionary[cachedISO8601DateFormatterKey];
-    
-    if (!dateFormatter) {
+
++ (NSISO8601DateFormatter *)dateFormatter {
+    static dispatch_once_t once;
+    static NSISO8601DateFormatter *dateFormatter;
+    dispatch_once(&once, ^{
         dateFormatter = [[NSISO8601DateFormatter alloc] init];
-        threadDictionary[cachedISO8601DateFormatterKey] = dateFormatter;
-    }
-    
-    return [dateFormatter stringFromDate:date];
+    });
+    return dateFormatter;
 }
 
++ (NSString *)ISO8601StringFromDate:(NSDate *)date {
+    return [[self dateFormatter] stringFromDate:date];
+}
 
-+ (NSDate *)dateFromTimeStamp:(NSTimeInterval) timestamp {
++ (NSDate *)dateFromTimeStamp:(NSTimeInterval)timestamp {
     if (!timestamp) {
         return [NSDate date];
     }
+    
     return [NSDate dateWithTimeIntervalSince1970:timestamp];
 }
 
